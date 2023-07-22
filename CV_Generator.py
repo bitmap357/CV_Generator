04a69cd8663ebdbda808a8153ad0621e173f306e
bitmap357 <edakoyeram@gmail.com>
2023-07-22 22:14:07 +0000

    email label and entry

--- CV_Generator.py
+++ CV_Generator.py
@@ -3,10 +3,15 @@ from tkinter import *
 window = Tk()
 window.title("CV Generator")
 
-label_name = Label(window, text="Name")
+label_name = Label(window, text="Name: ")
 label_name.pack()
 entry_name = Entry(window)
 entry_name.pack()
+
+label_email = Label(window, text="Email: ")
+label_email.pack()
+entry_email = Entry(window)
+entry_email.pack()
 
 
 window.mainloop()
